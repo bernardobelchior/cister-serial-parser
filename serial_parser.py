bd8bc6422994a91fb1eef9229fe6d03abd7766b9
Bernardo Belchior <bernardo.belchior1@gmail.com>
2017-11-28 09:20:47 +0000

    Added sandbox and serial_path options.

--- serial_parser.py
+++ serial_parser.py
@@ -2,42 +2,56 @@ import serial
 import asyncio
 import requests
 import json
+import sys
+import time
 from configparser import SafeConfigParser
 
-serial_path = '/dev/ttyUSB3'
+serial_path = '/dev/ttyUSB0'
+host = 'http://localhost:3000'
+sandbox = False
+
+if len(sys.argv) > 1:
+    serial_path = sys.argv[1]    
 serial_baudrate = 115200
 
 
 def read_config():
+    global host 
+    global sandbox 
+    global serial_path
+
     config = SafeConfigParser()
+
     config.read('config.ini')
-    return config.get('REST API', 'host')
+    serial_path = config.get('REST API', 'serial_path', fallback = '/dev/ttyUSB0')
+    host = config.get('REST API', 'host', fallback = 'http://localhost:3000')
+    sandbox = config.getboolean('REST API', 'sandbox', fallback = False)
 
 
-def read(host):
+def read():
     ser = serial.Serial(serial_path, serial_baudrate)
 
     while True:
         line = ser.readline().decode('ascii')
         split_line = line.split()
-        print(line)
         if split_line[0] == "Sensor:":
             try:
-                mote_id = int(split_line[1])
-                temperature = float(split_line[2])
-                humidity = float(split_line[3])
+                timestamp = time.time()
+                msg_id = int(split_line[1])
+                mote_id = int(split_line[2])
+                temperature = float(split_line[3])
+                humidity = float(split_line[4])
+                print("Message ID: {}\tMote ID: {}\tTemperature: {}\tHumidity: {}".format(msg_id, mote_id, temperature, humidity))
+
+                if not sandbox:
+                    send_request(host, timestamp, mote_id, temperature, humidity)
             except ValueError:
                 print("Error")
-            else:
-                print("ID: " + split_line[1] + "\tTemperature: " + split_line[2] + "\tHumidity: " + split_line[3])
 
-            send_request(host, mote_id, timestamp, temperature, humidity)
-
-
-async def send_request(host, mote_id, timestamp, temperature, humidity):
+def send_request(host, timestamp, mote_id, temperature, humidity):
     response = requests.put(
         host + '/addMeasurement',
-        headers={'Content-Type': 'application/json'},
+        headers={'content-type': 'application/json'},
         data=json.dumps({
             'moteId': str(mote_id),
             'timestamp': str(timestamp * 1000),
@@ -48,8 +62,8 @@ async def send_request(host, mote_id, timestamp, temperature, humidity):
 
 
 def start():
-    host = read_config()
-    read(host)
+    read_config()
+    read()
 
 
 start()
