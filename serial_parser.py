ca25138fa9c048176a0dc1b36cdcd52c1e98c924
Bernardo Belchior <bernardo.belchior1@gmail.com>
2017-12-04 09:46:54 +0000

    Script is now always running.

--- serial_parser.py
+++ serial_parser.py
@@ -12,23 +12,24 @@ sandbox = False
 debug = False
 
 if len(sys.argv) > 1:
-    serial_path = sys.argv[1]    
+    serial_path = sys.argv[1]
 serial_baudrate = 115200
 
 
 def read_config():
-    global host 
-    global sandbox 
+    global host
+    global sandbox
     global serial_path
     global debug
 
     config = SafeConfigParser()
 
     config.read('config.ini')
-    serial_path = config.get('REST API', 'serial_path', fallback = '/dev/ttyUSB0')
-    host = config.get('REST API', 'host', fallback = 'http://localhost:3000')
-    sandbox = config.getboolean('REST API', 'sandbox', fallback = False)
-    debug = config.getboolean('REST API', 'debug', fallback = False)
+    serial_path = config.get('REST API', 'serial_path',
+                             fallback='/dev/ttyUSB0')
+    host = config.get('REST API', 'host', fallback='http://localhost:3000')
+    sandbox = config.getboolean('REST API', 'sandbox', fallback=False)
+    debug = config.getboolean('REST API', 'debug', fallback=False)
 
 
 def read():
@@ -45,12 +46,15 @@ def read():
                 temperature = float(split_line[3])
                 humidity = float(split_line[4])
                 if debug:
-                    print("Message ID: {}\tMote ID: {}\tTemperature: {}\tHumidity: {}".format(msg_id, mote_id, temperature, humidity))
+                    print("Message ID: {}\tMote ID: {}\tTemperature: {}\tHumidity: {}".format(
+                        msg_id, mote_id, temperature, humidity))
 
                 if not sandbox:
-                    send_request(host, timestamp, mote_id, temperature, humidity)
+                    send_request(host, timestamp, mote_id,
+                                 temperature, humidity)
             except ValueError:
                 print("Error")
+
 
 def send_request(host, timestamp, mote_id, temperature, humidity):
     response = requests.put(
@@ -62,12 +66,15 @@ def send_request(host, timestamp, mote_id, temperature, humidity):
             'temperature': str(temperature),
             'humidity': str(humidity)
         }))
-    print(response)
 
 
 def start():
     read_config()
-    read()
+    while True:
+        try:
+            read()
+        except:
+            pass
 
 
 start()
